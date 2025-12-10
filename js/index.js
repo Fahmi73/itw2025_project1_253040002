b2d316cab91c8cc131c85720a422a00051ae253c
Fahmi73 <hamdanifahmi73@gmail.com>
2025-12-10 22:46:45 +0700

    menambahkan tombol, dan spiner animation di bagian form

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 const menuToggle = document.querySelector('.ri-menu-2-fill');
 const listMenu = document.querySelector('.nav_menu_toggle');
+const feedbackAlert = document.querySelector('.feedback');
+const closeBtn = document.querySelector('.closeAlert');
+
+closeBtn.addEventListener('click', () => {
+    feedbackAlert.classList.toggle('enabled');
+})
 
 menuToggle.addEventListener('click', () => {
     listMenu.classList.toggle('slide');

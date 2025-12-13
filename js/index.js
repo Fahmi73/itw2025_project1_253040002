3e5eb4c2e212b1554652be4697af5e1165bafba1
Fahmi73 <hamdanifahmi73@gmail.com>
2025-12-13 12:59:21 +0700

    fix_navbar dan responsivitas tablet 1024px dan 900px

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 const menuToggle = document.querySelector('.invis_checbox');
 const listMenu = document.querySelector('.nav_menu');
 const feedbackAlert = document.querySelector('.feedback');
-const closeBtn = document.querySelector('.closeAlert');
-
-closeBtn.addEventListener('click', () => {
-    feedbackAlert.classList.toggle('enabled');
-})
 
 menuToggle.addEventListener('click', () => {
     listMenu.classList.toggle('slide');
